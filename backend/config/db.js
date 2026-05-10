5a5c97abdd566206db382c43fe569de68ed6f2ed
ankitajena2003 <ankitajena2003>
2026-05-10 22:08:17 +0530

    Updated api.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD,
-  database: "personal_finance_tracker",
+  database: process.env.DB_NAME || "personal_finance_tracker",
 });
 
 db.connect((err) => {
